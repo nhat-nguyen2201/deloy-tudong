d6b388ce962e310e61ce201eea0c2133372e9862
nhatnguyen <phamvannhatnguyen@gmail.com>
2025-12-03 09:03:26 +0700

    acg

--- backend/index.php
+++ backend/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); // Cho phép frontend gọi từ domain khác rjehfkh
+header('Access-Control-Allow-Origin: *'); // Cho phép frontend gọi từ domain khác
 
 $data = [
     'message' => 'Hello from PHP backend!',
